850386450f7c9b226c47aaab59566935f109d803
eunwoo-kimm <144404516+eunwoo-kimm@users.noreply.github.com>
2024-02-28 18:55:01 +0900

    Update ert_main.c

--- ACC_stmr/ert_main.c
+++ ACC_stmr/ert_main.c
@@ -82,7 +82,7 @@ static void handler_100ms(int sig, siginfo_t *si, void *uc)
   }
 
     rt_OneStep();
-    printf("%4d: %f -> %f -> %f \n", c++ * 10,ACCsystem_DW.DiscreteTimeIntegrator_DSTATE, ACCsystem_B.DiscreteTimeIntegrator,rtb_Min);
+    printf("%4d: %f -> %f -> %f \n", c++ * 10,ACCsystem_DW.DiscreteTimeIntegrator_DSTATE, ACCsystem_B.DiscreteTimeIntegrator,ACCsystem_DW.RateTransition1_Buffer0);
 
   return;
 }
